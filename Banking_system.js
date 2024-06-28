09f50fc9f3f72cf3d4c05128fddf2894954c6508
EEB12 <rayhan.taqara@yahoo.com>
2024-06-28 19:36:13 +0700

    fix code

--- Banking_system.js
+++ Banking_system.js
@@ -1,115 +1,115 @@
 
-class bank{
+class bank {
 
 
-    constructor(Saldo){
-        this.Saldo=Saldo
+    constructor(Saldo) {
+        this.Saldo = Saldo
     }
 }
 
 
-class Rekening extends bank{
+class Rekening extends bank {
 
 
-    constructor(Saldo){
+    constructor(Saldo) {
         super(Saldo)
     }
 
-    deposit(value){
-        
-        return new Promise((resolve) =>{
+    deposit(value) {
+
+        return new Promise((resolve,reject) => {
             setTimeout(() => {
-                this.Saldo+=value
+                this.Saldo += value
                 this.CheckTransaction("deposit")
                 resolve()
-            },2000);
-            
+            }, 2000);
+
         });
-        
+
     }
-    withdraw(value){
-        return new Promise ((resolve)=>{
-            if(this.Saldo<value){
+    withdraw(value) {
+        return new Promise((resolve) => {
+            if (this.Saldo < value) {
                 alert(`You don't have insufficient balance `)
-               return
+                return
             }
 
-            setTimeout(()=>{
-                this.Saldo-=value
+            setTimeout(() => {
+                this.Saldo -= value
                 this.CheckTransaction("withdraw")
-                resolve
-            },2000)
-            
-
-
-        })
-       
-       
-    }
-
-    checkBalance(){
-
-        return new Promise ((resolve)=>{
-            setTimeout(()=>{
-                alert(`saldo anda adalah `+this.Saldo)
                 resolve()
-            },2000)
-            
+            }, 2000)
+        })
+
+
+    }
+
+    checkBalance() {
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(`saldo anda adalah ` + this.Saldo)
+                resolve()
+            }, 2000)
+
 
 
         })
-        
+
     }
-    CheckTransaction(information){
-        alert(`${information} Berhasil, saldo anda sekarang adalah `+this.Saldo)
+    CheckTransaction(information) {
+        alert(`${information} Berhasil, saldo anda sekarang adalah ` + this.Saldo)
     }
-    
+
 }
 
-let akun= new Rekening(0)
+let akun = new Rekening(0)
 let command
 let value
 
-async function main(){
-
-    while(isNaN(command) || command > 4 || command < 1 ){
-        value=NaN
+async function main() {
     
-        command=Number(window.prompt(`Pick command by typing the number:
+        while (isNaN(command) || command > 4 || command < 1) {
+            value = NaN
+
+            command = Number(window.prompt(`Pick command by typing the number:
         1. Withdraw
         2. Deposit
         3. Check Balance
         4. Exit`))
-        if(isNaN(command) || command > 4 || command < 1){
-            alert("please type valid number between 1 until 3 !!!")
-            continue
-        }
-        if(command == 3){
-            command=NaN
-            await akun.checkBalance()
-            continue
-        }
-        if(command == 4){
-            
-            alert("Terimakasih telah menggunakan bank_account.js")
-            Break
-        }
-        while(isNaN(value) || value<0){
-            value=Number(window.prompt("Please insert value"))
-            if(isNaN(value) || value<0){
-                alert("Input only accept number !!!")
+            if (isNaN(command) || command > 4 || command < 1) {
+                alert("please type valid number between 1 until 3 !!!")
                 continue
             }
-            else{
-                command==1?await akun.withdraw(value):command==2? await akun.deposit(value):""
-               
-                command=NaN
-                value=NaN
-                break
+            if (command == 3) {
+                command = NaN
+                await akun.checkBalance()
+                continue
             }
+            if (command == 4) {
+
+                alert("Terimakasih telah menggunakan bank_account.js")
+                Break
+            }
+            while (isNaN(value) || value < 0) {
+                value = Number(window.prompt("Please insert value"))
+                if (isNaN(value) || value < 0) {
+                    alert("Input only accept number !!!")
+                    continue
+                }
+                else {
+                    command == 1 ? await akun.withdraw(value).catch(error => alert(error)) : command == 2 ? await akun.deposit(value).catch(error => alert(error)) : ""
+
+                    command = NaN
+                    value = NaN
+                    break
+                }
+            }
+
         }
-        
-    }
+    
+  
+
 }
 
 
